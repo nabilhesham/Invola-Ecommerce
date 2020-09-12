a6a4f386ab46bf8bf904ad31f7906a953d83df1c
Nabil <spadecode94@gmail.com>
2020-09-12 21:09:36 +0200

    finish coffee pods view and filter

--- products/api/views.py
+++ products/api/views.py
@@ -55,6 +55,21 @@ def coffee_pod_data(request: Request):
     try:
         qs = CoffeePod.objects.all().values()
 
+        # product type filter
+        product_type = request.query_params.get("product_type", "")
+        if product_type:
+            qs = qs.filter(product_type=product_type)
+
+        # coffee flavor filter
+        coffee_flavor = request.query_params.get("coffee_flavor", "")
+        if coffee_flavor:
+            qs = qs.filter(coffee_flavor=coffee_flavor)
+
+        # pack size filter
+        pack_size = request.query_params.get("pack_size", "")
+        if pack_size:
+            qs = qs.filter(pack_size=pack_size)
+
         result["coffee_pods"] = qs
         return Response(status=status.HTTP_200_OK, data=result)
     except Exception as e:
